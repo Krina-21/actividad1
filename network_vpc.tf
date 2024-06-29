79f0f9d5d710c29fafa6586b67d6ade89b2a5af0
Karina Villamizar <87285403+Krina-21@users.noreply.github.com>
2024-06-29 18:31:24 -0500

    Update network_vpc.tf

--- network_vpc.tf
+++ network_vpc.tf
@@ -1,7 +1,7 @@
 
 # AWS provider configuration
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 # Main VPC
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main_vpc"
+    Name = "mi_vpc"
   }
 }
 
@@ -18,6 +18,6 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main_igw"
+    Name = "mi_igaws"
   }
 }
